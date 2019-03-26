6124b334b0aec8e51b13f1adc55a0422b4674057
benliu5085 <47131361+benliu5085@users.noreply.github.com>
2019-03-25 21:24:34 -0500

    Add files via upload

---
+++ proj2/train.py
@@ -0,0 +1,64 @@
+from HMM_log import *
+import time
+
+print("loading data...")
+data = []
+words = set([])
+with open("alllines.txt", 'r') as fin:
+    for line in fin:
+        sentence = line[1:-2].lower()
+        t = sentence.split()
+        for i in t:
+            if i[-1] < 'a' or i[-1] > 'z':
+                words.add(i[:-1])
+                data.append(i[:-1])
+            else:
+                words.add(i)
+                data.append(i)
+
+alphabet = list(words)
+print("loading done!")
+
+N = 10
+T = len(data)        # number of observations
+A = np.zeros((N, N))
+B = np.zeros((N, len(alphabet)))
+P = np.random.random_sample(N)
+
+"""random initialization"""
+P = P/P.sum()
+for i in range(0, B.shape[0]):
+    t = np.random.random_sample((B.shape[1]))
+    B[i,] = t/t.sum()
+
+for i in range(0, A.shape[0]):
+    t = np.random.random_sample((A.shape[1]))
+    A[i,] = t/t.sum()
+
+print("start training ...")
+time_start = time.time()
+(trP, emP, initP) = trainHMM(N, alphabet, data, 1, A, B, P)
+time_end = time.time()
+print(str(time_end - time_start) + " s")
+print("training done!")
+
+fout = open("hmm_parameter.txt",'w')
+fout.write(str(emP.shape[0]) + '\n')
+
+for it in alphabet:
+    fout.write(it + ' ')
+fout.write('\n')
+
+for x in initP:
+    fout.write(str(x) + ' ')
+fout.write('\n')
+
+for line in trP:
+    for x in line:
+        fout.write(str(x) + ' ')
+    fout.write('\n')
+for line in emP:
+    for x in line:
+        fout.write(str(x) + ' ')
+    fout.write('\n')
+fout.close()
